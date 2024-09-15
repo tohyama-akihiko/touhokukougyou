f96342bfbd9904d63f317605a7b9e6521e06cd41
ackyclash <ack570930@gmail.com>
2024-09-15 21:28:58 +0900

    サイト構築更新

--- WordPressTheme/functions.php
+++ WordPressTheme/functions.php
@@ -149,3 +149,9 @@ function my_excerpt_more( $more ) {
 
 }
 add_filter( 'excerpt_more', 'my_excerpt_more' );
+
+// ContactForm7で自動挿入されるPタグ、brタグを削除
+add_filter('wpcf7_autop_or_not', 'wpcf7_autop_return_false');
+function wpcf7_autop_return_false() {
+	return false;
+}
--- WordPressTheme/page-faq.php
+++ WordPressTheme/page-faq.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 <main>
-  <div class="page__header">
+  <section class="page__header">
     <div class="inner">
       <h1 class="page__titlewrap section-title">
         <span class="page__subtitle section__subtitle">●スタッフQ&A</span>
@@ -11,22 +11,76 @@
       </h1>
       <div class="breadcrumb"></div>
     </div>
-  </div>
-  <div class="page__header">
-    <h2 class="faq__title page__h2">スタッフQ＆A</h2>
-    <p class="page__header-text">Q&A形式で東北工業での仕事や職場の様子を先輩スタッフに語ってもらいました。写真やコメントを職場選びの参考にしてください。</p>
-  </div>
+  </section>
 
-  <ul class="accordion-area">
-    <li class="accordion">
-      <div class="accordion-title title bg01">
-        <p><span>Q.</span>どのくらい継続使用すると効果が感じられますか?</p>
+  <section class="faq">
+    <div class="faq__header">
+      <div class="inner">
+        <h2 class="faq__title page__h2">スタッフQ＆A</h2>
+        <p class="page__header-text">Q&A形式で東北工業での仕事や職場の様子を先輩スタッフに語ってもらいました。写真やコメントを職場選びの参考にしてください。</p>
       </div>
-      <div class="accordion-box box">
-        <p><span>A.</span>使用直後から潤いを感じていただけます。長期的に使用いただくことで、ダメージが修繕しツヤ・サラを感じていただけます。</p>
+    </div>
+
+    <div class="faq-slider">
+      <div><img src="<?php echo get_template_directory_uri(); ?>/assets/images/faq/slide_qa01.png" alt="" /></div>
+      <div><img src="<?php echo get_template_directory_uri(); ?>/assets/images/faq/slide_qa02.png" alt="" /></div>
+      <div><img src="<?php echo get_template_directory_uri(); ?>/assets/images/faq/slide_qa03.png" alt="" /></div>
+      <div><img src="<?php echo get_template_directory_uri(); ?>/assets/images/faq/slide_qa04.png" alt="" /></div>
+      <div><img src="<?php echo get_template_directory_uri(); ?>/assets/images/faq/slide_qa05.png" alt="" /></div>
+      <div><img src="<?php echo get_template_directory_uri(); ?>/assets/images/faq/slide_qa06.png" alt="" /></div>
+    </div>
+
+    <div class="accordion-wrap">
+      <div class="inner">
+        <ul class="accordion-area">
+          <li class="accordion">
+            <div class="accordion-title title">
+              <p><span class="question">Q.</span>どのくらい継続使用すると効果が感じられますか?どのくらい継続使用するかままま<span class="ball">●</span> </p>
+            </div>
+            <div class="accordion-box box">
+              <p><span class="answer">A.</span>使用直後から潤いを感じていただけます。長期的に使用いただくことで、ダメージが修繕しツヤ・サラを感じていただけます。</p>
+            </div>
+          </li>
+            <div class="accordion-title title">
+              <p><span class="question">Q.</span>どのくらい継続使用すると効果が感じられますか?どのくらい継続使用するかまままま<span class="ball">●</span> </p>
+            </div>
+            <div class="accordion-box box">
+              <p><span class="answer">A.</span>使用直後から潤いを感じていただけます。長期的に使用いただくことで、ダメージが修繕しツヤ・サラを感じていただけます。</p>
+            </div>
+          </li>
+        </ul>
       </div>
-    </li>
-  </ul>
+    </div>
+
+
+  </section>
+
+
+  <section class="know-more">
+    <div class="box02">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/top/circle01.png" alt="" class="js-rellax" />
+    </div>
+    <div class="inner">
+      <div class="know-more__header">
+        <h2 class="know-more__title page__h2">もっと知りたい！東北工業</h2>
+      </div>
+      <div class="recruit__content">
+        <a href="" class="recruit__item">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/top/recruit02.png" alt="スタッフQ&A" class="recruit__image">
+          <span class="recruit__link">スタッフQ&A</span>
+        </a>
+        <a href="" class="recruit__item">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/top/recruit04.png" alt="応募者の皆さんへ" class="recruit__image">
+          <span class="recruit__link">会社概要</span>
+        </a>
+        <a href="" class="recruit__item">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/top/recruit03.png" alt="数字で見る東北工業" class="recruit__image">
+          <span class="recruit__link">数字で見る東北工業</span>
+        </a>
+      </div>
+    </div>
+  </section>
+
 
 
 
